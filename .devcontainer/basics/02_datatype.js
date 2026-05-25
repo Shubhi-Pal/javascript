1a7f4ed5a3cd8bfcf62d6a7a966e77887e8959a1
Shubhi Pal <shubhipaldob2007@gmail.com>
2026-05-25 01:44:55 +0000

    datatypes

---
+++ .devcontainer/basics/02_datatype.js
@@ -0,0 +1,20 @@
+"Use strict";// treat all js code as newer version
+//alert(4+4)// we are using nodejs,not browser
+console.log(3+3)
+console.log('shubhi')
+let name = "shubhi"
+let age = 20
+let isLoggedIn = false
+
+
+//number=> 2 to power 53
+//bigint
+//string=>""
+//boolean=> true/false
+//null=> standalone value
+//undefined=>
+//symbol=> unique
+//object
+console.log(typeof "shubhi");
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object
