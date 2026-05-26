dc94a64cc4561c5f03cb1b09e36498a03025a1af
Shubhi Pal <shubhipaldob2007@gmail.com>
2026-05-26 02:38:16 +0000

    conversion

---
+++ .devcontainer/basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score = "33"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+//"33"=> 33
+//"33abc"=> NaN
+// true =>1; false=>0
+
+//let IsLoggedIn =1
+//let booleanIsloggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/3);
+console.log(2%3);
+let str1 ="hello"
+let str2 ="shubhi"
+let str3 = str1+str2
+console.log(str3);
+console.log("1"+2+2);
+console.log(1+2+"2");
