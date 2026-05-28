5368649d20c6887ebb3a75299d5fe94e50bb0545
Shubhi Pal <shubhipaldob2007@gmail.com>
2026-05-28 01:38:59 +0000

    datatype_

--- .devcontainer/basics/03_conversionOperation.js
+++ .devcontainer/basics/03_conversionOperation.js
@@ -29,3 +29,11 @@ let str3 = str1+str2
 console.log(str3);
 console.log("1"+2+2);
 console.log(1+2+"2");
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+console.log(undefined == 0);
+console.log(undefined>0);
+console.log(undefined<0);
+//===
---
+++ .devcontainer/basics/datatype_summary.js
@@ -0,0 +1,31 @@
+// Primitive
+// 7 types : String,Number,boolean,null,undefined,Symbol,BigInt
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+const bigNumber = 3455678993224n
+
+
+
+//Reference (Non Primitive)
+
+//Array,Objects,functions
+const heros = ["shaktiman","doga","naagraj"]
+ let myObj={
+    name:"shubhi",
+    age: 21,
+}
+const myFunction = function(){
+    console.log("hello world");
+}
+
+console.log(typeof bignumber);
+console.log(typeof score)
